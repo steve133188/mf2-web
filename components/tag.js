ecfe2c407057f4c7461d4a2843dec1fe5dfc62be
Lapson Wong <lapsonwor@gmail.com>
2021-11-07 18:47:43 +0800

    fix tag component

--- components/tag.js
+++ components/tag.js
@@ -3,7 +3,7 @@ import '../styles/components/tag.scss';
 
 export function tag(props) {
     return (
-        <span className={"tag tag_text tag_"+(props.is_team ? "team_" : "_")+props.order}>
+        <span className={"tag tag_text tag_"+(props.is_team ? "team_" : "")+props.order}>
             {props.text}
         </span>
     )
