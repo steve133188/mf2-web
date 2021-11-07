ff5a2c11f41712a108742598640670ab33b2c06e
stevechak <steve133188@gmail.com>
2021-11-07 19:45:40 +0800

    config input height unit to vh

--- pages/login/index.js
+++ pages/login/index.js
@@ -37,8 +37,9 @@ export default function Login(){
                             <img className={styles.login_logo} src="MS_logo-square.svg" alt=""/>
                         <div className={styles.welcomeMessage}>
                             {errors?<h4 className={"red-text"}>{errors}</h4>:null}
-                            <h4 className={styles.login_title}>Log In</h4>
-                            <p>Welcome back! Login with your data that <br/> you entered during registration</p>
+                            <h4 className={"title_text"}>Log In</h4>
+                            {/*<h4 className={styles.login_title}>Log In</h4>*/}
+                            <p className={"regular_text "}>Welcome back! Login with your data that <br/> you entered during registration</p>
                         </div>
                     </div>
                     <div className={"login-form"}>
@@ -73,7 +74,7 @@ export default function Login(){
                             <Link href="/login/recovery"><a><span className={styles.forgotPassword}>Forgot Password?</span></a></Link>
                         </div>
 
-                        <div className={"submit-row"}>
+                        <div className={styles.submit_row}>
                             <button className={"login-btn align-self-center"} disabled={!validateForm()} onClick={handleSubmit}>
                                 Login
                             </button>
