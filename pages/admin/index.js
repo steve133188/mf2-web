8bca48064312b081e3a9c633c647bd313d122244
steve133188 <steve133188@gmail.com>
2021-12-01 15:29:00 +0800

    add fix delete many customers problem

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,21 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { SearcBox} from "../../components/Input";
-import {
-    CancelButton,
-    SelectButton,
-    AddButton,
-    ConfirmButton
-} from "../../components/Button";
 import {useRouter} from "next/router";
-import {PaginationControlled} from "../../components/Pagination";
-import {useState, useEffect} from "react";
-
-import {useTheme} from "@mui/material/styles";
-import {InnerSidebar} from "../../components/InnerSidebar";
-import { Checkbox } from '../../components/Checkbox';
-
-import * as userApi from "../../api/UserAPI";
 
 
 export default function Admin() {
@@ -26,6 +9,7 @@ export default function Admin() {
     return (
         <div>
             <div className="admin_layout">
+
               </div>
         </div>
     )
