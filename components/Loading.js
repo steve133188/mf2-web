1f2eae8024fcdf11f40ecfeca237405ee9f2304c
tim-ck <tim.ckchow@gmail.com>
2021-12-21 16:22:09 +0800

    add components cancel confirmation

--- components/Loading.js
+++ components/Loading.js
@@ -1,12 +1,8 @@
-import {Avatar} from "./Icon";
-import SideBar from "../layouts/SideBar";
 import Skeleton from "@mui/material/Skeleton";
 import * as React from "react";
 
 export default function NotificationList({notification , ...props}){
     const {state} = props;
-//when click the notification, set unreadCount to 0
-    //const {isFinish} = props;
     return(
         <div className={state}>
             <div className="loader">
