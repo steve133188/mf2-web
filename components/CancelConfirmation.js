35288087e67b74c257e4d15f98db14e7aaf4565e
tim-ck <tim.ckchow@gmail.com>
2021-12-23 13:28:18 +0800

    add contact delete confirmation and loading scene for contact.js

--- components/CancelConfirmation.js
+++ components/CancelConfirmation.js
@@ -1,19 +1,30 @@
 import * as React from "react";
 
 export default function NotificationList({notification , ...props}){
-
+    const {onConfirm, onCancel} = props;
+    const handleConfirm= () => {
+        onConfirm();
+    };
+    const handleCancel = () => {
+        onCancel();
+    };
     return(
-        <div className={"confirmationBox"}>
-            <div className="content">
-                <h2>
-                Are you sure you wish to delete this item?
+        <div className="popup">
+            <div className={"confirmationBox"}>
+                <div className="content">
+                    <h2>
+                    Are you sure you wish to delete this item?
 
-                </h2>
-            </div>
-            <div className="btnGroup">
-                <button>Confirm</button>
-                <button className={"mf_bg_light_grey mf_color_text"}>Cancel</button>
+                    </h2>
+                </div>
+                <div className="btnGroup">
+                    <button onClick={() => handleConfirm()}>Confirm</button>
+                    <button onClick={() => handleCancel()} className={"mf_bg_light_grey mf_color_text"}>Cancel</button>
+                </div>
             </div>
         </div>
-    )
+
+    );
+
+
 }
