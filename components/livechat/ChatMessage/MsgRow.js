5d78ac4d119252e5b8aa913a993ab9efbd24f00a
steve133188 <steve133188@gmail.com>
2022-01-03 13:09:02 +0800

    no message

--- components/livechat/ChatMessage/MsgRow.js
+++ components/livechat/ChatMessage/MsgRow.js
@@ -15,12 +15,12 @@ const captionJson = JSON.stringify(imageCaption)
 export default function MsgRow({msg ,isSearch,refProp,...props}){
 
     const [isImageOpen,setImageOpen] = useState(false)
-
+    const {user}=useContext(GlobalContext)
     const messageType = (msg)=>{
         switch(msg.message_type){
             case "sticker": return (<div className={"msg_type_sticker"}><img className={"imageBox"} src={msg.media_url}/></div>);
             case "image":   return <div className={"msg_type_image"}><img className={"imageBox"} src={msg.media_url}/></div> ;
-            case "imageCaption":   
+            case "imageCaption":
                 return <>
                         {/* {console.log(JSON.parse(captionJson).url)} */}
                                 {/* JSON.parse({msg.body}) */}
@@ -30,24 +30,24 @@ export default function MsgRow({msg ,isSearch,refProp,...props}){
                                     <div value={msg.timestamp} className={"imageMessage"}>{JSON.parse(captionJson).message}</div>
                                 </div>
                         </>
-            case "video":   return <div className={"msg_type_video"}> 
+            case "video":   return <div className={"msg_type_video"}>
                     {/* <iframe allowFullScreen className={"videoBox"} src={msg.body}  /> */}
                     <Player value={msg.timestamp} className={"videoBox"} playsInline fluid={false} width={360} muted={true}>
                         <BigPlayButton position="center" />
                                     <source value={msg.timestamp} src={msg.media_url}   type="video/mp4"/>
                     </Player>
-                    </div> 
+                    </div>
             case "ptt":   return <div className={"msg_type_voice "+"reply"}>
                                             <audio value={msg.timestamp} className={"voice_detail"} controls src={msg.media_url} ></audio>
 
                         </div>;
             case "document":
                 return  <div className={"msg_type_attachment"}>
-                    <div className={"attachmentBox"}> 
+                    <div className={"attachmentBox"}>
                         <svg xmlns="http://www.w3.org/2000/svg"  width="35" height="35" viewBox="0 0 35 35" >
                                 <g id="Mask_Group_62" data-name="Mask Group 62" transform="translate(-2746 -1111)" >
                                     <rect id="Background-2" data-name="Background" width="35" height="35" transform="translate(2746 1111)" fill="none"/>
-                                    <path id="Path_34405" data-name="Path 34405" d="M973.181,507.951h0a1.148,1.148,0,0,0-1.1,1.1v14.067a5.318,5.318,0,0,1-4.667,5.362c-.175.017-.348.026-.519.026a5.141,5.141,0,0,1-5.13-4.651,4.806,4.806,0,0,1-.028-.52V506.919a3.306,3.306,0,0,1,2.849-3.349,3.222,3.222,0,0,1,.333-.017,3.168,3.168,0,0,1,3.156,2.846,2.922,2.922,0,0,1,.017.338v13.951a1.189,1.189,0,0,1-1.222,1.144h0a1.19,1.19,0,0,1-1.145-1.14V509.048a1.144,1.144,0,0,0-1.182-1.1h0a1.146,1.146,0,0,0-1.1,1.1v11.438a3.6,3.6,0,0,0,3.1,3.644,3.468,3.468,0,0,0,3.81-3.09c.012-.12.017-.242.017-.364V506.956a5.607,5.607,0,0,0-4.927-5.656c-.18-.017-.359-.026-.536-.026a5.436,5.436,0,0,0-5.429,4.93c-.016.174-.023.348-.025.523v16.3a7.655,7.655,0,0,0,6.723,7.737c.247.025.493.036.735.036a7.431,7.431,0,0,0,7.416-6.736c.023-.241.035-.484.035-.728V509.048A1.144,1.144,0,0,0,973.181,507.951Z" transform="translate(1796.697 612.626)" className={"attachmentPinLogo"} fill="#2198fa"/>     
+                                    <path id="Path_34405" data-name="Path 34405" d="M973.181,507.951h0a1.148,1.148,0,0,0-1.1,1.1v14.067a5.318,5.318,0,0,1-4.667,5.362c-.175.017-.348.026-.519.026a5.141,5.141,0,0,1-5.13-4.651,4.806,4.806,0,0,1-.028-.52V506.919a3.306,3.306,0,0,1,2.849-3.349,3.222,3.222,0,0,1,.333-.017,3.168,3.168,0,0,1,3.156,2.846,2.922,2.922,0,0,1,.017.338v13.951a1.189,1.189,0,0,1-1.222,1.144h0a1.19,1.19,0,0,1-1.145-1.14V509.048a1.144,1.144,0,0,0-1.182-1.1h0a1.146,1.146,0,0,0-1.1,1.1v11.438a3.6,3.6,0,0,0,3.1,3.644,3.468,3.468,0,0,0,3.81-3.09c.012-.12.017-.242.017-.364V506.956a5.607,5.607,0,0,0-4.927-5.656c-.18-.017-.359-.026-.536-.026a5.436,5.436,0,0,0-5.429,4.93c-.016.174-.023.348-.025.523v16.3a7.655,7.655,0,0,0,6.723,7.737c.247.025.493.036.735.036a7.431,7.431,0,0,0,7.416-6.736c.023-.241.035-.484.035-.728V509.048A1.144,1.144,0,0,0,973.181,507.951Z" transform="translate(1796.697 612.626)" className={"attachmentPinLogo"} fill="#2198fa"/>
                                 </g>
                             </svg>
                         <a className={"attachmentBody"} href='/msg.body.fileURL' download>{msg.body}</a>
@@ -55,7 +55,7 @@ export default function MsgRow({msg ,isSearch,refProp,...props}){
                      </div>;
             case "link": return <div className={"url_body"}>
                     <div className={"url_box"}>
-                        <div className={"url_detail"} >                        
+                        <div className={"url_detail"} >
                              <Embed   url={msg.body} />
                              </div>
 
@@ -69,18 +69,18 @@ export default function MsgRow({msg ,isSearch,refProp,...props}){
 
         }
     }
-   
+
     return(
         <div className={"msg_row"} id={msg.timestamp} >
             <div className={msg.from_me?"msg_from_me":"msg_from_other"}>
                 <div>{messageType(msg)}</div>
                 {/* <div className={"msg_body"}>{msg.body}</div> */}
                 {/* {props.d.map(e=>{return msg.sender==e.phone?console.log("I find it",e.phone):msg.sender})} */}
-                
-                {msg.sender&&<div className={"msg_timestamp"}>+{msg.sender.slice(0,3)} {msg.sender.slice(3)}__{new Date(parseInt(msg.timestamp*1000)).toLocaleTimeString()} </div>}
+
+                {/*{msg.sender&&msg.from_me&&<div className={"msg_timestamp"}>+{msg.sender.slice(0,3)} {msg.sender.slice(3)}__{new Date(parseInt(msg.timestamp*1000)).toLocaleTimeString()} </div>}*/}
+                {msg.sender&&msg.from_me&&<div className={"msg_timestamp"}>{user.user.username} _ {new Date(parseInt(msg.timestamp*1000)).toLocaleTimeString()} </div>}
             </div>
         </div>
     )
 }
 
- 
