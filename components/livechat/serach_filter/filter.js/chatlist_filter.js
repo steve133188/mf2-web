ca4e3cf5af8023dc0fb6797d24d4d9e8ecf240c8
alanroro <95203444+alanroro@users.noreply.github.com>
2022-01-04 16:14:33 +0800

    svg

--- components/livechat/serach_filter/filter.js/chatlist_filter.js
+++ components/livechat/serach_filter/filter.js/chatlist_filter.js
@@ -13,12 +13,13 @@ import DropDown from "../../../filter/teamDropDown";
 import DivisionDropDown from "../../../filter/divisionDropDown";
 
 export default function ChatlistFilter( {...props}){
-    const channelData = [
-        // {name:"All Channel",value:"All",id:0},
-                {name:"WhastApp",value:"whatsapp"},
-                {name:"WhatsApp Business",value:"WABA"},
-                {name:"Messager",value:"messager"},
-                {name:"WeChat",value:"wechat"},];
+
+const channelData = [
+    // name:"WhastApp",value:"All",channelID:"All",id:0},
+            {name:"WhastApp",value:"Whatsapp",channelID:"Whatsapp",id:1},
+            {name:"WhatsApp Business",value:"WABA",channelID:"WhatsappB",id:2},
+            {name:"Messager",value:"Messager",channelID:"Messager",id:3},
+            {name:"WeChat",value:"Wechat",channelID:"Wechat",id:4},];
 
                 const [selectedTags ,setSelectedTags] =useState([])
                 const { userInstance ,tagInstance ,orgInstance, user} = useContext(GlobalContext);
