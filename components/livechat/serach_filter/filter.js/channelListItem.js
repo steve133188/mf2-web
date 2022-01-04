baac811885e4e7437f2355544bf6e9706b802c9d
alanroro <95203444+alanroro@users.noreply.github.com>
2022-01-04 15:59:40 +0800

    svg

--- components/livechat/serach_filter/filter.js/channelListItem.js
+++ components/livechat/serach_filter/filter.js/channelListItem.js
@@ -8,7 +8,7 @@ export default function ChannelListItem(  props){
         <div className={"channelListitem"}>
                         <div className={"left"}>
                        
-                    <img key={"id"} width="32px" height="32px" src={`/channel_SVG/${props.channel}.svg`}  hidden={false}  alt=""/>
+                    <img key={"id"} width="20px" height="20px" src={`/channel_SVG/${props.channel}.svg`}  hidden={true}  alt=""/>
                         <div style={{margin:"0 5px", minWidth:"180px"}}> {props.name}</div>
                         </div>
                         <div className={"right"}>
