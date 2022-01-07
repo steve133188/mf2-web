85878567d21a10fe2f561b13f9e29dcc0eb30066
tim-ck <tim.ckchow@gmail.com>
2022-01-07 15:32:21 +0800

    change loading component from skeleton to svg

--- components/Loading.js
+++ components/Loading.js
@@ -5,9 +5,7 @@ export default function NotificationList({notification , ...props}){
     const {state} = props;
     return(
         <div className={state}>
-            <div className="loader">
-                <h1><Skeleton sx={{ bgcolor: '#0ABAB5' }}/></h1>
-            </div>
+            <div className="loader"/>
         </div>
     )
 }
