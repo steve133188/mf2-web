31429af4b8fe5a413b69557061fd5e04cd2a0096
alanroro <95203444+alanroro@users.noreply.github.com>
2022-01-14 15:55:27 +0800

    timer rebuild

--- components/CountDownTimer.js
+++ components/CountDownTimer.js
@@ -9,35 +9,47 @@ export default function CountDownTimer ({dayString,trigger}) {
     
     //add 24 hour
     useEffect(()=>{
-        const Time = parseInt((dayString))*1000+86400000
+        const Time = (parseInt(dayString)+86400)*1000
         const lastTime= Time -  Date.parse(new Date())
-        
-        setEndTime( lastTime+57600000   )
+        setEndTime( lastTime  )
     },[dayString])
     
     useEffect(()=>{
-        console.log(endTime,"endtime testeing")
+
         if(!endTime||endTime<0) return setTimeLeft("00:00:00");
         
                 const timerInt = setInterval(()=> {
                     setEndTime(endTime-1000)
-
-                    setTimeLeft(new Date(endTime).toLocaleTimeString())
+                    setTimeLeft(getTimeRemaining(endTime))
                     // setTimeLeft(endTime)
                 },1000)
-        console.log("timer,build")
         return ()=>clearInterval(timerInt)
     },[endTime])
 
     // const endTime = new Date( lastTime)
     //function calculate the time difference between now and the end time and return it as a string
+    function getTimeRemaining(endTime) {
+        
+        if(endTime> 0){
+        const seconds = Math.floor((endTime / 1000) % 60);
+        const minutes = Math.floor((endTime / 1000 / 60) % 60);
+        const hours = Math.floor((endTime / (1000 * 60 * 60)) % 24);
+        const ans= hours+":"+minutes+":"+seconds;
+        // return total;
+        return ans;
+        }else if(total <0){
+        return "00:00:00";}
+        else{
+            return 
+        }
+    }
 
 
     return(
        <div className={"confirmationBox"}>
                 {/* {endTime.toString()} */}
                 {/* <h1>{timeLeft}</h1> */}
-                <div className={'tim_timer'} style={{height:"18px"}}>{timeLeft??"00:00:00"}</div>
+                <div className={'tim_timer'} style={{height:"18px"}}>{timeLeft}</div>
             </div>
     )
 
