3c4ee9ac82346c23d8e1f35f1b9367cce3e6f4f2
alanroro <95203444+alanroro@users.noreply.github.com>
2022-01-17 17:41:43 +0800

    connect_whatsapp.js

--- components/integrations/connect_whatsapp.js
+++ components/integrations/connect_whatsapp.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react"
 import QRCode from "qrcode.react";
 
 export default function ConnectWhatsapp(props){
-    const ori = `1@ELVX50MU59V6dKw/y/42AEyetp2qdA8xzKXqteOtTIghzwizzMlLuwADOHpWTt5ve0Hr7l/GCmddeg==,RIK+NqFiHRAeb/sd2bwqjunNCzlMBvCcN9iCdF0pkSM=,EGkQKkTo0rQzBF/vbuxhfg==`
-    const [qrCodeState,setQrCodeState] = useState(ori)
+    // const oriData = `1@ELVX50MU59V6dKw/y/42AEyetp2qdA8xzKXqteOtTIghzwizzMlLuwADOHpWTt5ve0Hr7l/GCmddeg==,RIK+NqFiHRAeb/sd2bwqjunNCzlMBvCcN9iCdF0pkSM=,EGkQKkTo0rQzBF/vbuxhfg==`
+    const oriData = ""
+    const [qrCodeState,setQrCodeState] = useState(oriData)
     const [refresh,setStateRefresh] = useState(false)
     useEffect(()=>{
         // if(!props.fetchData){return}
@@ -11,9 +12,10 @@ export default function ConnectWhatsapp(props){
         fetchData()
     },[props.fetchdata])
     useEffect(()=>{
-        setQrCodeState()
+
+        setQrCodeState(oriData)
         setStateRefresh(!refresh)
-    },[ori])
+    },[oriData])
 
     const fetchData = ()=>{
         console.log("connect whatsapp ")
@@ -24,8 +26,8 @@ export default function ConnectWhatsapp(props){
     return(<>
     <div className="intergra_container">
         <p>Scan the QR code with your phone and keep your phone connected to internet.</p>
-        {false?<div style={{display:"flex",flexDirection:'column'}}>
-            <img src={qrCodeState} style={{width:"100px",margin:"30px auto"}} />
+        { oriData ==""?<div style={{display:"flex",flexDirection:'column'}}>
+            <img src={"/channel_SVG/Whatsapp.svg"} style={{width:"100px",margin:"30px auto"}} />
 
                 Waiting for QR Authorization.
         </div>
