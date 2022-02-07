421a4ecc98216829859f3cb6561c1c7cbb730d1a
steve133188 <steve133188@gmail.com>
2022-02-08 04:40:02 +0800

    Adjusted the row rendering

--- components/livechat/ChatMessage/MsgRow.js
+++ components/livechat/ChatMessage/MsgRow.js
@@ -8,13 +8,7 @@ import {API, graphqlOperation} from "aws-amplify";
 import {getMessageByMsgId} from "../../../src/graphql/queries";
 
 
-const imageCaption = {
-    url:"https://p0.pikrepo.com/preview/876/531/orange-tabby-cat-sitting-on-green-grasses-selective-focus-photo.jpg",
-    message:"hahaha"
-}
-const captionJson = JSON.stringify(imageCaption)
-
-export default function MsgRow({msg, isSearch, refProp, replyHandle, confirmReply, confirmForward, ...props}) {
+export default function MsgRow({msg, refProp, replyHandle, confirmReply, confirmForward, ...props}) {
 
     const {user} = useContext(GlobalContext)
     const [isLoading , setIsLoading]  = useState(true)
@@ -30,9 +24,7 @@ export default function MsgRow({msg, isSearch, refProp, replyHandle, confirmRepl
         console.log(result);
         return result
     };
-    const replyclick = e => {
-        replyHandle(e.target.id);
-    }
+
 
     const getQuoteMsg = async (msg_id) => {
         const data =await API.graphql(graphqlOperation(getMessageByMsgId, {message_id: msg_id})).then(res => {
@@ -59,81 +51,47 @@ export default function MsgRow({msg, isSearch, refProp, replyHandle, confirmRepl
     }
 
     const messageType = (msg) => {
-        // console.log(msg.room_id,"msg.room_id msg.room_id",msg.room_id,"sadasdsad",msg.timestamp)
         switch (msg.message_type) {
             case "replyMsg":
                 return <div id={"reply" + msg.timestamp} value={msg.timestamp}
                             className={"msg_type_image" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}></div>;
             case "sticker":
-                return (<div className={"msg_type_sticker" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}>
-                    {props.replyMsg == msg.timestamp ?
-                        <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                    <img id={msg.timestamp} onClick={replyclick} value={msg.timestamp} className={"imageBox"}
-                         src={msg.media_url}/>
-                </div>);
-
+                return <img id={msg.timestamp}  value={msg.timestamp} className={"imageBox"} src={msg.media_url}/>;
             case "IMAGE":
             case "image":
                 if (msg.body) {
-                    return <div id={msg.timestamp} onClick={replyclick} value={msg.timestamp}
-                                className={"msg_type_imageCaption" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                        <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                        {/* {console.log(JSON.parse(captionJson).url)} */}
-                        {/* JSON.parse({msg.body}) */}
-                        {/* // <div value={msg.timestamp}  className={"msg_type_imageCaption"}> */}
-                        {/* <img className={"bigImageBox"} src={msg.media_url} onClick={()=>{setImageOpen(!isImageOpen)}} style={isImageOpen?{ display:'block'}:{display:'none'}}/> */}
+                    return <div id={msg.timestamp} value={msg.timestamp}
+                                className={"msg_type_imageCaption" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}>
+                        {props.replyMsg&&<Reply confirmReply={confirmReply} confirmForward={confirmForward}/>}
                         <img id={msg.timestamp} className={"imageBox"} src={msg.media_url} onClick={() => {
                             setImageOpen(!isImageOpen)
                         }}/>
-                        <div id={msg.timestamp} onClick={replyclick} value={msg.timestamp}
-                             className={"imageMessage"}>{msg.body}</div>
+                        <div id={msg.timestamp} value={msg.timestamp}
+                             className={"imageMessage"}><div className={"content"}>{msg.body}</div> </div>
                     </div>
+                }else{
+                    return <img id={msg.timestamp}className={"imageBox"}src={msg.media_url}/>
                 }
-                return <div id={msg.timestamp} onClick={replyclick}
-                            className={"msg_type_image" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                    <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}<img id={msg.timestamp}
-                                                                                                   className={"imageBox"}
-                                                                                                   src={msg.media_url}/>
-                </div>;
-            // case "imageCaption":
-            //     return <div id={msg.timestamp} onClick={replyclick} className={"msg_type_imageCaption"  +( props.replyMsg==msg.timestamp?" replyActive":"")}> {props.replyMsg==msg.timestamp?<Reply confirmReply={confirmReply} confirmForward={confirmForward}/>:""}
-            //             {/* {console.log(JSON.parse(captionJson).url)} */}
-            //                     {/* JSON.parse({msg.body}) */}
-            //                     {/* // <div value={msg.timestamp}  className={"msg_type_imageCaption"}> */}
-            //                         <img className={"bigImageBox"} src={JSON.parse(captionJson).url}  onClick={()=>{setImageOpen(!isImageOpen)}} style={isImageOpen?{ display:'block'}:{display:'none'}}/>
-            //                         <img id={msg.timestamp} className={"imageBox"} src={JSON.parse(captionJson).url} onClick={()=>{setImageOpen(!isImageOpen)}}/>
-            //                         <div id={msg.timestamp} onClick={replyclick}  className={"imageMessage"}>{JSON.parse(captionJson).message}</div>
-            //                     </div>
-
             case "VIDEO":
             case "video":
-                return <div id={msg.timestamp}
-                            className={"msg_type_video" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                    <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                    {/* <iframe allowFullScreen className={"videoBox"} src={msg.body}  /> */}
+                return <>
                     <Player className={"videoBox"} playsInline fluid={false} width={360} muted={true}>
                         <BigPlayButton position="center"/>
                         <source src={msg.media_url} type="video/mp4"/>
                     </Player>
-                    <div id={msg.timestamp} onClick={replyclick} value={msg.timestamp}
+                    <div id={msg.timestamp}  value={msg.timestamp}
                          className={"imageMessage"}>{msg.body}</div>
-                </div>
+                    </>;
+
             case "ptt":
             case "voice" :
-                return <div id={msg.timestamp} onClick={replyclick} value={msg.timestamp}
-                            className={"msg_type_voice " + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                    <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                    <audio id={msg.timestamp} onClick={replyclick} value={msg.timestamp} className={"voice_detail"}
-                           controls src={msg.media_url}></audio>
-                </div>;
+                return <audio id={msg.timestamp}  value={msg.timestamp} className={"voice_detail"} controls src={msg.media_url}></audio>;
+
             case "FILE":
             case "file":
             case "document":
-                return <div id={msg.timestamp} onClick={replyclick} value={msg.timestamp}
-                            className={"msg_type_attachment" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                    <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                    <div id={msg.timestamp} onClick={replyclick} className={"attachmentBox"}>
-                        <svg id={msg.timestamp} onClick={replyclick} xmlns="http://www.w3.org/2000/svg" width="35"
+                return <div id={msg.timestamp}  className={"attachmentBox msg_type_attachment"}>
+                        <svg id={msg.timestamp}  xmlns="http://www.w3.org/2000/svg" width="35"
                              height="35" viewBox="0 0 35 35">
                             <g id="Mask_Group_62" data-name="Mask Group 62" transform="translate(-2746 -1111)">
                                 <rect id="Background-2" data-name="Background" width="35" height="35"
@@ -144,91 +102,46 @@ export default function MsgRow({msg, isSearch, refProp, replyHandle, confirmRepl
                                       fill="#2198fa"/>
                             </g>
                         </svg>
-                        {/*<a id={msg.timestamp}  className={"attachmentBody"} href={msg.media_url} download>{msg.body||msg.media_url.slice(0,25)+"..."}</a>*/}
                         <a id={msg.timestamp} className={"attachmentBody"} href={msg.media_url} download>{msg.body}</a>
-                        <div className={"attachmentSize"}>{msg.media_url.slice(-4)}</div>
-                    </div>
-                </div>;
-            case "link":
-                return <div id={msg.timestamp} onClick={replyclick}
-                            className={"url_body" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                    <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                    <div className={"url_box"}>
-                        {/* <div className={"url_detail"} >
-                                            <Embed   url={msg.body} />
-                                            </div> */}
-                        <div id={msg.timestamp} onClick={replyclick} value={msg.timestamp}
-                             className={"msg_type_url" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                            <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                            <a href={msg.body}>{msg.body} </a>
+                        </div>;
 
-                        </div>
-                    </div>
-                </div>;
+            case "link":
+                return <a href={msg.body}>{msg.body} </a> ;
 
             default:
-                return (
-                    // isValidURL(msg.body) ?
-                    //     <div id={msg.timestamp} onClick={replyclick}  className={"url_body"  +( props.replyMsg==msg.timestamp?" replyActive":"")}> {props.replyMsg==msg.timestamp?<Reply confirmReply={confirmReply} confirmForward={confirmForward}/>:""}
-                    //             <div className={"url_box"}>
-                    //                 {/* <div className={"url_detail"} >
-                    //                     <Embed   url={msg.body} />
-                    //                     </div> */}
-                    //                 <div  value={msg.timestamp} className={"msg_type_url" }> {props.replyMsg==msg.timestamp?<Reply confirmReply={confirmReply} confirmForward={confirmForward}/>:""}
-                    //                     <p id={msg.timestamp} onClick={replyclick} >
-                    //                         <a  href={msg.body} >{msg.body} </a>
-                    //                         </p>
-                    //                 </div>
-                    //             </div>
-                    // </div>
-                    // :
-                    <div id={msg.timestamp} onClick={replyclick} value={msg.timestamp}
-                         className={isSearch ? "msg_body_highligh" : "msg_body" + (props.replyMsg == msg.timestamp ? " replyActive" : "")}> {props.replyMsg == msg.timestamp ?
-                        <Reply confirmReply={confirmReply} confirmForward={confirmForward}/> : ""}
-                        {/* {`${isValidURL(msg.body)}`} </div> */}
-                        {(msg.body)} </div>
-                )
-            // default:   return  <div value={msg.timestamp} className={isSearch?"msg_body_highligh":"msg_body"}>{msg.body}</div>
+                return <div className={"content"}>{msg.body}</div>
+
         }
     }
 
 
     return (
-        <div className={"msg_row"} id={msg.timestamp}>
-            <div className={msg.from_me ? "msg_from_me" : "msg_from_other"}>
+        <div className={"msg_row "+(msg.from_me&&"msg_from_me")} id={msg.timestamp}>
+            {/*Message bubble*/}
+            {/*Message body start*/}
+            <div className={"msg_body "}>
                 {msg.hasQuotedMsg ?
-                    <div style={{
-                        width: "fit-content",
-                        maxWidth: "50%",
-                        padding: "1%",
-                        borderRadius: "10px",
-                        marginLeft: msg.from_me ? "auto" : "0",
-                        backgroundColor: (msg.from_me ? "#D0E9FF" : "white")
-                    }}>
+                    <>
                         <div className={msg.hasQuotedMsg ? 'reply_box' : ""}>{isLoading ?"wait for content":messageType(quote)}</div>
                         <div className={msg.hasQuotedMsg ? 'reply' : ""}>{messageType(msg)}</div>
-                    </div> :
-                    <div>{messageType(msg)}</div>}
-
-                {/* <div className={"msg_body"}>{msg.body}</div> */}
-                {/* {props.c.map(e=>{return msg.sender==e.phone?console.log("I find it",e.phone):msg.sender})} */}
-
-                {/*{msg.sender&&msg.from_me&&<div className={"msg_timestamp"}>+{msg.sender.slice(0,3)} {msg.sender.slice(3)}__{new Date(parseInt(msg.timestamp*1000)).toLocaleTimeString()} </div>}*/}
-                {msg.sender && msg.from_me && <div
-                    className={"msg_timestamp"}>{msg.sign_name}&ensp; {new Date(parseInt(msg.timestamp * 1000)).toDateString('en-US', {
+                    </>
+                    : messageType(msg)}
+            </div>
+            {/*Message body end*/}
+            {/*Timestamp start*/}
+            {/*<div className={"msg_footer"}>*/}
+                    <div
+                    className={"msg_timestamp"}>
+                        {msg.sender && msg.from_me && msg.sign_name}
+                        &ensp; {new Date(parseInt(msg.timestamp * 1000)).toDateString('en-US', {
                     hour: '2-digit',
                     minute: '2-digit'
                 })} &ensp; {new Date(parseInt(msg.timestamp * 1000)).toLocaleTimeString('en-US', {
                     hour: '2-digit',
                     minute: '2-digit'
-                })} </div>}
-                {/*{msg.sender&&!msg.from_me&&<div className={"msg_timestamp"}>{props.c.filter(e=>{return parseInt(msg.sender)==e.customer_id?true:false})[0].name} &ensp;  {new Date(parseInt(msg.timestamp*1000)).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })} </div>}*/}
-                {msg.sender && !msg.from_me && <div
-                    className={"msg_timestamp"}> &ensp;  {new Date(parseInt(msg.timestamp * 1000)).toLocaleTimeString('en-US', {
-                    hour: '2-digit',
-                    minute: '2-digit'
-                })} </div>}
-            </div>
+                })} </div>
+            {/*</div>*/}
+            {/*Timestamp end*/}
         </div>
     )
 }
